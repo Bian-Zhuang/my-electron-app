1ca8c64d144c6a9a49beef1178cf4adbd9fe08bb
BZ <2417619531@qq.com>
2022-12-21 21:51:30 +0800

    feactor: 重构渲染进程

--- preload.js
+++ preload.js
@@ -1,11 +1,8 @@
-// DOMContentLoaded 当初始的 HTML 文档被完全加载和解析完成之后
-window.addEventListener('DOMContentLoaded', () => {
-  const replaceText = (selector, text) => {
-    const element = document.getElementById(selector);
-    if (element) element.innerText = text;
-  };
-
-  for (const dependency of ['chrome', 'node', 'electron']) {
-    replaceText(`${dependency}-version`, process.versions[dependency]);
-  }
+const { contextBridge } = require('electron');
+// 通过 contextBridge 接口定义 全局对象
+contextBridge.exposeInMainWorld('versions', {
+  node: () => process.versions.node,
+  chrome: () => process.versions.chrome,
+  electron: () => process.versions.electron,
+  // 能暴露的不仅仅是函数，我们还可以暴露变量
 });
